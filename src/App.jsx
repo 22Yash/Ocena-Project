5ce6e894217378bad7da2570fce7cb2bdcba0d8e
22Yash <yash.223234108@vcet.edu.in>
2024-04-20 18:18:06 +0530

    done with these task

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import FormComponent from './components/FormComponent'
+import "./App.css"
+import { useState } from "react";
+import { BtechPDF, MtechPDF } from "./pdf.jsx";
+import { PDFViewer } from "@react-pdf/renderer";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [name, setName] = useState('');
+  const [course, setCourse] = useState('');
+  const [pdfContent, setPdfContent] = useState(null);
+  const currentDate = new Date().toLocaleDateString();
+
+  const generatePdf = () => {
+    let content;
+  
+    if (course === 'B.tech') {
+      content = <BtechPDF name={name} date={currentDate} />;
+      console.log('===>>',content)
+    } else if (course === 'M.tech') {
+      content = <MtechPDF name={name} date={currentDate} />;
+    }
+
+    setPdfContent(content);
+  };
+
+  const handleInputChange = (e) => {
+    setPdfContent(null);
+    setName(e.target.value);
+  };
 
   return (
-    <>
-    <FormComponent/>
-    </>
-  )
+    <div className="App">
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" value={name} onChange={handleInputChange} />
+        </div>
+        <div>
+          <label htmlFor="course">Course:</label>
+          <select id="course" value={course} onChange={(e) => setCourse(e.target.value)}>
+            <option value="">Select Course</option>
+            <option value="B.tech">B.tech</option>
+            <option value="M.tech">M.tech</option>
+          </select>
+        </div>
+        <div>
+          <button type="button" onClick={generatePdf} className="p-10 w-40 h-20 pt-4 -pl-2">
+            Generate PDF
+          </button>
+        </div>
+      </form>
+      {pdfContent && (
+        <div style={{ marginTop: '20px' }}>
+          <PDFViewer width="1000" height="600">
+            {pdfContent}
+          </PDFViewer>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
